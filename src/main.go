d72e2aef16b1e74f01fb9df465d1269f65f3505f
hideyuki-aoki <aoki.ba4@gmail.com>
2019-12-04 01:43:25 +0900

    modify add return value

--- src/main.go
+++ src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func hello() string {
-	return "Hello Katsuo"
+func hello() (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello Katsuo",
+	}, nil
 }
 
 func main() {
